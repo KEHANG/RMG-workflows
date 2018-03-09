1de45e654bd97f05559b3e9ddb1d70b9021b0dfa
KEHANG <kehanghan@gmail.com>
2018-03-09 15:37:48 -0500

    add conda path to PATH for crontab jobs

--- cnn_workflows/crontabs/nightly_cnn_build.sh
+++ cnn_workflows/crontabs/nightly_cnn_build.sh
@@ -1,3 +1,7 @@
+#########################################
+# Add conda path to PATH (user specific)#
+#########################################
+# export PATH=/your/conda/bin:$PATH
 
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
