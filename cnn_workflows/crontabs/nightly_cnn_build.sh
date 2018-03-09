0f37cbcd1ed5473183ffb049237aad0f39db9621
KEHANG <kehanghan@gmail.com>
2018-03-09 16:44:20 -0500

    it turns out you can set PATH in "crontab -e" window
    put SHELL=...
    put PATH=...same as your terminal PATH (copy over)...
    so that crontab would behavior the way you
    launch calculation manually

--- cnn_workflows/crontabs/nightly_cnn_build.sh
+++ cnn_workflows/crontabs/nightly_cnn_build.sh
@@ -1,13 +1,3 @@
-#########################################
-# Add conda path to PATH (user specific)#
-#########################################
-# export PATH=/your/conda/bin:$PATH
-
-####################################
-# Add other necessary path to PATH #
-# (e.g., slave node's sbatch)      #
-####################################
-
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
 
 cd ${BASE_DIR}
